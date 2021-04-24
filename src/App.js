36b12d15428e0cb3188ee75f60ae54f275b27547
Noevenzahn <info.artuofficial@gmail.com>
2021-04-24 14:10:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+
+
+class App extends React.Component {
+    state = {
+        advice: ""
+    }
+
+    componentDidMount() {
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios.get("https://api.adviceslip.com/advice")
+        .then ((response) => {
+            const { advice } = response.data.slip; // this destructuring works like const advice = response.data.slip.advice;
+                console.log(advice);
+                this.setState({ advice }) // works like this.setState({ advice: advice })
+        })
+        .catch((error) => {
+            console.error();
+        })
+    }
+
+    render() {
+        const { advice } = this.state; // works like advice = this.state.advice
+        return (
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>Advice, please!</span>
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
+
+// Übung: random background (unsplash API?)
+// Übung: refactor this to a function component
+// Übung: use fetch() instead of axios
+// Übung: use more APIs!
